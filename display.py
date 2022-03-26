1197ca49ec1014190bac8431fadcc758fe62e2a0
Stefan Urosevic <stefanuros@gmail.com>
2022-03-26 01:06:21 -0700

    Add screen update optimization

--- display.py
+++ display.py
@@ -13,6 +13,7 @@ import time
 
 textSize = 25
 subtextSize = 15
+
 prevMinute = 0
 now = datetime.now()
 
@@ -21,7 +22,9 @@ def init():
   global screen, text
 
   screen = Papirus()
-  text = PapirusTextPos()
+  text = PapirusTextPos(False)
+  
+  text.Clear()
   
   # TODO Move this to the screen switching code
   initStatus()
@@ -33,18 +36,21 @@ def main():
     # Get current time
     now = datetime.now()
     
-    # Update the frame
+    # Update the screen
     statusUpdate()
     
+    text.WriteAll()
+    
     # Sleep
-    time.sleep(5)
+    time.sleep(1)
     
     # Update values
     prevMinute = now.minute
   
 def initStatus():
-  text.AddText(text="##:## XX", x=5, y=5, size=textSize, Id="Time")
-  text.AddText(text="XXXXX, XXX ##", x=5, y=textSize + 10, size=subtextSize, Id="Date")
+  text.AddText(text="##:## XX", x=5, y=10, size=textSize, Id="Time")
+  text.AddText(text="XXXXX, XXX ##", x=5, y=textSize + 20, size=subtextSize, Id="Day")
+  text.AddText(text="XXXXX, XXX ##", x=5, y=textSize + 20 + subtextSize, size=subtextSize, Id="Date")
   
 def deInitStatus():
   text.RemoveText("Time")
@@ -52,8 +58,9 @@ def deInitStatus():
 
 def statusUpdate():
   if(prevMinute != now.minute): 
-    currentTime = now.strftime("%I:%M:%S %p")
-    currentDate = now.strftime("%A, %b %d")
+    currentTime = now.strftime("%H:%M")
+    currentDay = now.strftime("%A")
+    currentDate = now.strftime("%b %d")
     
     text.UpdateText("Time", currentTime)
     text.UpdateText("Date", currentDate)
