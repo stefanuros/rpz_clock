b42e81294e19feb296fd52ae5027feacf3009bbb
Stefan Urosevic <stefanuros@gmail.com>
2022-03-26 22:03:26 -0700

    Will the fonts work now?

--- display.py
+++ display.py
@@ -12,6 +12,11 @@ updateFrame = False
 prevMinute = -1
 now = datetime.now()
 
+# boldFontPath = "./fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf"
+# regularFontPath = "./fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf"
+boldFontPath = "./fonts/Courier_Prime/CourierPrime-Bold.ttf"
+regularFontPath = "./fonts/Courier_Prime/CourierPrime-Regular.ttf"
+
 
 def init():
   global text
@@ -54,9 +59,9 @@ def initStatus():
   global updateFrame
   
   print("Status Init")
-  text.AddText(text=now.strftime("%H:%M"), x=5, y=10, size=textSize, Id="Time", fontPath="./fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf")
-  text.AddText(text=now.strftime("%A"), x=5, y=textSize + 20, size=subtextSize, Id="Day", fontPath="./fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf")
-  text.AddText(text=now.strftime("%b %d"), x=5, y=textSize + 20 + subtextSize, size=subtextSize, Id="Date", fontPath="./fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf")
+  text.AddText(text=now.strftime("%H:%M"), x=5, y=10, size=textSize, Id="Time", fontPath=boldFontPath)
+  text.AddText(text=now.strftime("%A"), x=5, y=textSize + 20, size=subtextSize, Id="Day", fontPath=regularFontPath)
+  text.AddText(text=now.strftime("%b %d"), x=5, y=textSize + 20 + subtextSize, size=subtextSize, Id="Date", fontPath=regularFontPath)
   
   updateFrame = True
 
@@ -71,9 +76,9 @@ def statusUpdate():
     currentDay = now.strftime("%A")
     currentDate = now.strftime("%b %d")
     
-    text.UpdateText("Time", currentTime)
-    text.UpdateText("Day", currentDay)
-    text.UpdateText("Date", currentDate)
+    text.UpdateText("Time", currentTime, boldFontPath)
+    text.UpdateText("Day", currentDay, regularFontPath)
+    text.UpdateText("Date", currentDate, regularFontPath)
 
 if __name__ == "__main__":
   try:
