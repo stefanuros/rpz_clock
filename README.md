70660ca821086d6e0bba7484d7376dbff26da470
Stefan Urosevic <stefanuros@gmail.com>
2022-03-26 23:23:38 -0700

    Changed some stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rpz_status
 Clock and other status display for an E-ink display for the Raspberry Pi Zero
+
+## Setup
+1. https://github.com/PiSupply/PaPiRus
+2. `pip install geocoder`
+3. `pip install Pillow`
--- display.py
+++ display.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
-from papirus import Papirus
-from papirus import PapirusTextPos
+from papirus import Papirus, PapirusComposite, LM75B
 from datetime import datetime
 import time
+from keys import weatherApiKey
+import geocoder
+from PIL import Image, ImageDraw, ImageFont
 
-textSize = 35
-subtextSize = 15
+TEXT_SIZE = 35
+SUBTEXT_SIZE = 17
+
+REGULAR_FONT_PATH = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+BOLD_FONT_PATH = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf"
+
+# REGULAR_FONT = ImageFont.truetype(REGULAR_FONT_PATH, TEXT_SIZE)
+# REGULAR_CHAR_WIDTH = REGULAR_FONT.getsize("0")[0]
+# BOLD_FONT = ImageFont.truetype(BOLD_FONT_PATH, SUBTEXT_SIZE)
+# BOLD_CHAR_WIDTH = BOLD_FONT.getsize("0")[0]
+
+WHITE, BLACK = 1, 0
 
 updateFrame = False
 
 prevMinute = -1
 now = datetime.now()
 
-boldFontPath = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf"
-regularFontPath = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
-
-
 def init():
-  global text
+  global text, screen, image, draw, sensor
   
   print("Starting...")
 
-  text = PapirusTextPos(False)
+  text = PapirusComposite(False)
   text.partialUpdates = True
+  screen = Papirus()
+  text.partialUpdates = True
+  image = Image.new('1', screen.size, WHITE)
+  draw = ImageDraw.Draw(image)
+  
   
   # TODO Move this to the screen switching code
   initStatus()
   
 def deinit():
-  # TODO text has no attribute clear
-  text.clear()
+  text.Clear()
   
 def main():
   global now, prevMinute, updateFrame
@@ -52,14 +64,13 @@ def main():
     # Sleep
     time.sleep(1)
     
-  
 def initStatus():
   global updateFrame
   
   print("Status Init")
-  text.AddText(text=now.strftime("%H:%M"), x=5, y=10, size=textSize, Id="Time", fontPath=boldFontPath)
-  text.AddText(text=now.strftime("%A"), x=5, y=textSize + 20, size=subtextSize, Id="Day", fontPath=regularFontPath)
-  text.AddText(text=now.strftime("%b %d"), x=5, y=textSize + 20 + subtextSize, size=subtextSize, Id="Date", fontPath=regularFontPath)
+  text.AddText(text=now.strftime("%H:%M"), x=5, y=5, size=TEXT_SIZE, Id="Time", fontPath=BOLD_FONT_PATH)
+  text.AddText(text=now.strftime("%A"), x=5, y=TEXT_SIZE + 15, size=SUBTEXT_SIZE, Id="Day", fontPath=REGULAR_FONT_PATH)
+  text.AddText(text=now.strftime("%b %d"), x=5, y=TEXT_SIZE + 15 + SUBTEXT_SIZE, size=SUBTEXT_SIZE, Id="Date", fontPath=REGULAR_FONT_PATH)
   
   updateFrame = True
 
@@ -74,9 +85,9 @@ def statusUpdate():
     currentDay = now.strftime("%A")
     currentDate = now.strftime("%b %d")
     
-    text.UpdateText("Time", currentTime, boldFontPath)
-    text.UpdateText("Day", currentDay, regularFontPath)
-    text.UpdateText("Date", currentDate, regularFontPath)
+    text.UpdateText("Time", currentTime, BOLD_FONT_PATH)
+    text.UpdateText("Day", currentDay, REGULAR_FONT_PATH)
+    text.UpdateText("Date", currentDate, REGULAR_FONT_PATH)
 
 if __name__ == "__main__":
   try:
