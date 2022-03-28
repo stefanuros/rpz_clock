20a95d40a9d82d24d9b4172524c13f1d8d1201a7
Stefan Urosevic <stefanuros@gmail.com>
2022-03-28 10:40:15 -0700

    Add vertically centered date

--- display.py
+++ display.py
@@ -35,8 +35,8 @@ SMALL_CHAR_WIDTH = SMALL_FONT.getsize("0")[0]
 
 SPACING = 5
 
-WEATHER_ICON_SIZE = 50
-WEATHER_ICON_BASE = "./weather_icons/"
+WEATHER_ICON_SIZE = 48
+WEATHER_ICON_BASE_PATH = "./weather_icons/"
 
 SCREEN_WIDTH = 200
 SCREEN_HEIGHT = 96
@@ -49,61 +49,65 @@ prevMinute = -1
 prevHour = -1
 now = datetime.now()
 
-currentWeatherIcon = "icons8-sun-48.png"
+weatherData = {
+  "currentWeatherIcon": None,
+  "currentWeatherImage": None
+}
 
 def init():
   global epd, image, draw
-  
+
   if(DEBUG):
     print("Starting...")
 
   # composite = PapirusComposite(False)
-  
+
   epd = Papirus()
   image = Image.new('1', epd.size, WHITE)
   draw = ImageDraw.Draw(image)
-  
+
   # TODO Move this to the screen switching code
   initStatus()
-  
+
 def deinit():
   epd.clear()
   # composite.Clear()
-  
+
 def main():
   global now, prevMinute, prevHour, firstUpdate
-  
+
   while(True):
-    # Get current time
-    now = datetime.now()
-    
-    # Clear the screen
-    draw.rectangle([(0,0), (SCREEN_WIDTH, SCREEN_HEIGHT)], fill=WHITE, outline=WHITE)
-    
-    # Update the screen
-    statusUpdate()
-    
     if prevMinute != now.minute or firstUpdate:
       if(DEBUG):
         print("Main Update")
-      firstUpdate = False
-      
+
+      # Clear the screen
+      draw.rectangle([(0,0), (SCREEN_WIDTH, SCREEN_HEIGHT)], fill=WHITE, outline=WHITE)
+
+      # Update the screen
+      statusUpdate()
+
       # Do a full update every hour
       # composite.WriteAll(now.hour == prevHour)
-      
+
       epd.display(image)
       if(now.hour == prevHour):
         epd.partial_update()
       else:
         epd.update()
+        
+      firstUpdate = False
 
     # Update values
     prevMinute = now.minute
     prevHour = now.hour
-      
+
+    # Get current time
+    now = datetime.now()
+
     # Sleep
     time.sleep(1)
-    
+
 def initStatus():  
   if(DEBUG):
     print("Status Init")
@@ -116,37 +120,43 @@ def initStatus():
   # composite.AddText(text="", x=130, y=WEATHER_ICON_SIZE + 30, size=SMALL_SIZE, Id=IDS.TEMP_RANGE.value, fontPath=REGULAR_FONT_PATH)
 
 def statusUpdate():
-  # Time Update
-  if(prevMinute != now.minute or firstUpdate): 
-    if(DEBUG):
-      print("Status Update")
-    
-    currentTime = now.strftime("%H:%M")
-    currentDay = now.strftime("%A")
-    currentDate = now.strftime("%b %d")
-    
-    # if DEBUG:
-    #   print(len(currentTime) * HEADING_CHAR_WIDTH + (2*SPACING))
-    
-    draw.text((SPACING, SPACING), currentTime, fill=BLACK, font=HEADING_FONT)
-    
-    # Find midpoint of the vertical space left
-    
-    
-    draw.text((SPACING, (2*SPACING) + HEADING_SIZE), currentDay, fill=BLACK, font=REGULAR_FONT)
-    draw.text((SPACING, (3*SPACING) + HEADING_SIZE + REGULAR_SIZE), currentDate, fill=BLACK, font=REGULAR_FONT)
-    
-    # composite.UpdateText(IDS.TIME.value, currentTime, BOLD_FONT_PATH)
-    # composite.UpdateText(IDS.DAY.value, currentDay, REGULAR_FONT_PATH)
-    # composite.UpdateText(IDS.DATE.value, currentDate, REGULAR_FONT_PATH)
   
-  # Weather Update
+    # Weather Update
   if(now.minute % 30 == 0 or firstUpdate):
+    if DEBUG:
+      print("Weather Data Update")
+    # Past weather icon
     # composite.UpdateImg(IDS.WEATHER_ICON.value, WEATHER_ICON_BASE + currentWeatherIcon)
     # composite.UpdateText(IDS.TEMP.value, "21\u00b0C", REGULAR_FONT_PATH)
     # composite.UpdateText(IDS.TEMP_RANGE.value, "17\u00b0C/21\u00b0C", REGULAR_FONT_PATH)
     # draw.
-    pass
+    
+    weatherData["currentWeatherIcon"] = WEATHER_ICON_BASE_PATH + "icons8-sun-48.png"
+    weatherData["currentWeatherImage"] = Image.open(weatherData["currentWeatherIcon"]).resize((WEATHER_ICON_SIZE, WEATHER_ICON_SIZE))
+  
+  # Draw Update
+  if(prevMinute != now.minute or firstUpdate): 
+    if(DEBUG):
+      print("Status Update")
+    
+    # Update the time
+    currentTime = now.strftime("%H:%M")
+    currentDay = now.strftime("%A")
+    currentDate = now.strftime("%b %d")
+    
+    # Draw the time
+    draw.text((SPACING, SPACING), currentTime, fill=BLACK, font=HEADING_FONT)
+    draw.text((SPACING, SPACING + HEADING_SIZE + SPACING), currentDay, fill=BLACK, font=REGULAR_FONT)
+    draw.text((SPACING, SPACING + HEADING_SIZE + SPACING + REGULAR_SIZE), currentDate, fill=BLACK, font=REGULAR_FONT)
+    
+    # composite.UpdateText(IDS.TIME.value, currentTime, BOLD_FONT_PATH)
+    # composite.UpdateText(IDS.DAY.value, currentDay, REGULAR_FONT_PATH)
+    # composite.UpdateText(IDS.DATE.value, currentDate, REGULAR_FONT_PATH)
+
+    # Draw the weather
+    timeWidth = HEADING_CHAR_WIDTH * len(currentTime)
+    weatherIconX = (SCREEN_WIDTH - timeWidth - SPACING - WEATHER_ICON_SIZE)//2 + (SPACING * 2 + timeWidth)
+    image.paste(weatherData["currentWeatherImage"], (weatherIconX, SPACING)) 
 
 if __name__ == "__main__":
   try:
